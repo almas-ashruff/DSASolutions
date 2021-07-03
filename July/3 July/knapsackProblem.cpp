b4d24a1e19808d8a52dd68b920ec2f2852f6f6bd
Almas Khan <almasashruff@gmail.com>
2021-07-03 21:51:55 +0530

    July 3 - 1

--- July/3 July/knapsackProblem.cpp
+++ July/3 July/knapsackProblem.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 // Solved using dynamic programming O(Number of items * Capacity) space and time
 
+
+vector<vector<int>> getKnapsackItems(vector<vector<int>> knapsackValues, vector<vector<int>> items, int weight);
 vector<vector<int>> knapsackProblem (vector<vector<int>> items, int capacity) {
     vector<vector<int>> knapsackValues(items.size() + 1, vector<int>(capacity + 1, 0));
     // stores the maximum value that can be held in the knapsack of capacity column, 
@@ -14,4 +16,45 @@ vector<vector<int>> knapsackProblem (vector<vector<int>> items, int capacity) {
     // first row contains the value 0 as it represents the case when an items array of size 0 is given.
 
     
+    for(int i = 1; i < items.size() + 1; i++) {
+        int currentValue = items[i - 1][0];
+        int currentWeight = items[i - 1][1];
+
+        for(int c = 0; c < capacity + 1; c++) {
+            if(currentWeight > c) {
+                knapsackValues[i][c] = knapsackValues[i - 1][c];
+            } else {
+                knapsackValues[i][c] = max(knapsackValues[i - 1][c], knapsackValues[i - 1][c - currentWeight] + currentValue);
+            }
+        }
+    }
+	 // return currentValues[items.size()[capacity]; -> if you just wanna return the capacity
+
+    return getKnapsackItems(knapsackValues, items, knapsackValues[items.size()][capacity]);
+    
+}
+
+vector<vector<int>> getKnapsackItems(vector<vector<int>> knapsackValues, vector<vector<int>> items, int weight) {
+    vector<vector<int>> solution = {{}, {}};
+
+    int i = knapsackValues.size() - 1;
+    int c = knapsackValues[0].size() - 1;
+
+    while( i > 0) {
+    	if(knapsackValues[i][c] == knapsackValues[i - 1][c]) {
+            i--; // current item is not included;
+        } else {
+        	solution[1].insert(solution[1].begin(), i - 1);
+
+            c = c - items[i - 1][1];
+            i--;
+        }
+
+            if(c == 0) {
+                break;
+            }
+        }
+    solution[0].push_back(weight);
+
+    return solution; 
 }
---
+++ July/3 July/sortKSortedArray.cpp
@@ -0,0 +1,106 @@
+#include <vector>
+using namespace std;
+
+// Sort an array that is K sorted - every element is at max k distance from its actual position
+
+// We keep a window of current element and the next k elements. 
+// At every index, we fill the smallest element from that index.
+// to get the smallest index in that window at optimal complexity, we use a min heap
+
+
+class MinHeap {
+public:
+  vector<int> heap;
+
+  MinHeap(vector<int> array) { heap = buildHeap(array); }
+
+  vector<int> buildHeap(vector<int> array) {
+    int firstParentIdx = (array.size() - 2) / 2;
+		for(int currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--){
+			siftDown(currentIdx, array.size() - 1, array);
+		}
+		return array;
+  }
+
+  void siftDown(int currentIdx, int endIdx, vector<int> &heap) {
+    int childOneIdx = currentIdx * 2 + 1;
+		while(childOneIdx <= endIdx){
+			int childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+			int idxToSwap;
+			if(childTwoIdx != -1 && heap[childTwoIdx] < heap[childOneIdx]){
+				idxToSwap = childTwoIdx;
+			} else {
+				idxToSwap = childOneIdx;
+			}
+			if(heap[idxToSwap] < heap[currentIdx]){
+				swap(currentIdx, idxToSwap, heap);
+				currentIdx = idxToSwap;
+				childOneIdx = currentIdx * 2 + 1;
+			} else {
+				return;
+			}
+		}
+  }
+	
+	bool isEmpty() { return heap.size() == 0; }
+	
+  void siftUp(int currentIdx, vector<int> &heap) {
+    int parentIdx = (currentIdx - 1) / 2;
+		while(currentIdx > 0 && heap[currentIdx] < heap[parentIdx]){
+			swap(currentIdx, parentIdx, heap);
+			currentIdx = parentIdx;
+			parentIdx = (currentIdx - 1) / 2;
+		}
+  }
+
+  int peek() {
+    return heap[0];
+  }
+
+  int remove() {
+    swap(0, heap.size() - 1, heap);
+		int valueToRemove = heap.back();
+		heap.pop_back();
+		siftDown(0, heap.size() - 1, heap);
+		return valueToRemove;
+  }
+
+  void insert(int value) {
+    heap.push_back(value);
+		siftUp(heap.size() - 1, heap);
+  }
+	
+	void swap(int i, int j, vector<int> &heap) {
+		int temp = heap[j];
+		heap[j] = heap[i];
+		heap[i] = temp;
+	}
+};
+
+	
+
+
+vector<int> sortKSortedArray(vector<int> array, int k) {
+  vector<int> minHeapInputArray(array.begin(), array.begin() + min(k + 1, (int) array.size()));
+	auto minHeapWithKElements = new MinHeap(minHeapInputArray);
+	
+	int nextIndexToInsertElement = 0;
+	
+	for(int idx = k + 1; idx < array.size(); idx++) {
+		auto minElement = minHeapWithKElements -> remove();
+		array[nextIndexToInsertElement] = minElement;
+		nextIndexToInsertElement++;
+		
+		auto currentElement = array[idx];
+		minHeapWithKElements -> insert(currentElement);
+	}
+	
+	while(!minHeapWithKElements -> isEmpty()) {
+		auto minElement = minHeapWithKElements -> remove();
+		array[nextIndexToInsertElement] = minElement;
+		nextIndexToInsertElement++;
+	}
+	
+	return array;
+
+}
